c23395db8121ff71afba293fcb819b68dab8f953
MEOL23 <m.elizaoliveira123@gmail.com>
2025-02-20 21:09:02 +0000

    Carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,18 +5,18 @@
 int main() {                            //informações salvas da Carta 1
    
    
-    char estado = 'A';                 // Letra de ''A'' a ''H''.
-    char codigo[5] = "A01";               // Código: ''Estado'' + 01,02 etc. EX: A01
-    char nome[20] = "Salvador";        // Nome da cidade
-    float area = 693.8;                // Área em km²
-    float pib = 62.954;                // PIB da cidade
-    int pontosturisticos= 15;          // Quantidade de pontos turísticos
-    int populacao = 2400000;           // População da cidade
+    char estado = 'B';                 // Letra de ''A'' a ''H''.
+    char codigo[5] = "B02";               // Código: ''Estado'' + 01,02 etc. EX: A01
+    char nome[20] = "Belém";        // Nome da cidade
+    float area = 1.059;                // Área em km²
+    float pib = 34.4;                // PIB da cidade
+    int pontosturisticos= 17;          // Quantidade de pontos turísticos
+    int populacao = 1300000;           // População da cidade
 
 
 
-    printf("Cartas Super Trunfo\n"); 
-    printf("Carta 1\n");
+    printf("Cartas Super Trunfo\n");          
+    printf("Carta 2\n");
     printf("Estado: %c\n", estado);
     
     printf("Nome da Cidade: %s\n", nome);
@@ -30,6 +30,7 @@ int main() {                            //informações salvas da Carta 1
     printf("População: %d\n", populacao);
 
     printf ("Pontos Turísticos: %d\n", pontosturisticos);
+     
     
     
     
